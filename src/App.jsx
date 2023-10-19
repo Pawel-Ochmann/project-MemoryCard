5d6b46a248bf1209f6fff0eb4f4a0baa649828e6
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-10-19 21:34:32 +0200

    Return after few days break, get visual shape, add pokeBox event handler function as well as score and best score and local storage logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,56 @@ import './main.scss';
 import getPokemons from './pokemonList';
 
 function App() {
-  const [pokemon, setPokemon] = useState(null);
   const [pokeArray, setPokeArray] = useState([]);
+  const [pokeClicked, setPokeClicked] = useState([]);
+  const [bestScore, setBestScore] = useState(
+    parseInt(localStorage.getItem('bestScore', 10)) || 0
+  );
 
   // eslint-disable-next-line react/prop-types
   function PokemonBox({ name, pokemonImage }) {
     return (
-      <div className='pokeBox'>
-        <h2>{name}</h2>
+      <div
+        className='pokeBox'
+        onClick={() => {
+          clickPokemon(name);
+        }}
+      >
         <img src={pokemonImage} alt={name} />
+        <h2>{name}</h2>
       </div>
     );
   }
 
+  const clickPokemon = (key) => {
+    if (
+      pokeClicked.find((e) => {
+        return e === key;
+      })
+    ) {
+      localStorage.setItem('bestScore', pokeClicked.length);
+      location.reload();
+    }
+
+    const clickedArray = [...pokeClicked];
+    clickedArray.push(key);
+    setPokeClicked(clickedArray);
+  };
+
+  const resetBestScore = (e)=> {
+    e.preventDefault();
+    localStorage.setItem('bestScore', 0);
+    location.reload();
+  }
+
+
   useEffect(() => {
-    const pokeList = getPokemons(5);
+    const pokeList = getPokemons(6);
     const fetchData = async () => {
       const dataPromises = pokeList.map(async (pokeName) => {
         try {
           const response = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${pokeName}`
+            `https://pokeapi.co/api/v2/pokemon/${encodeURIComponent(pokeName)}`
           );
           if (!response) {
             throw new Error('Could not fetch pokemon image');
@@ -36,7 +66,7 @@ function App() {
       });
 
       const results = await Promise.all(dataPromises);
-    
+
       const filteredData = results.filter((data) => data !== null);
 
       setPokeArray(filteredData);
@@ -44,40 +74,34 @@ function App() {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/diglett')
-      .then((response) => response.json())
-      .then((data) => {
-        setPokemon(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
   return (
-    <div>
-      {pokemon ? (
-        <div>
-          <h2>{pokemon.name}</h2>
-          <img
-            src={pokemon.sprites.other.home.front_default}
-            alt={pokemon.name}
-          />
+    <>
+      <header>
+        <h1>Pokemon Memory Game</h1>
+        <p>
+          Get points by clicking on an image but don't click on any more than
+          once!
+        </p>
+        <div className='scoreBox'>
+          <p>Actual score:{pokeClicked.length}</p>
+          <p>Best score: {bestScore}</p>
+          <button onClick={resetBestScore}>Reset best score</button>
         </div>
-      ) : (
-        <p>Loading Pokémon data...</p>
-      )}
-      {pokeArray.map((pokemon) => {
-        return (
-          <PokemonBox
-            key={pokemon.name}
-            name={pokemon.name}
-            pokemonImage={pokemon.sprites.other.home.front_default}
-          ></PokemonBox>
-        );
-      })}
-    </div>
+      </header>
+      <main>
+        <div className='pokeContainer'>
+          {pokeArray.map((pokemon) => {
+            return (
+              <PokemonBox
+                key={pokemon.name}
+                name={pokemon.name}
+                pokemonImage={pokemon.sprites.other.home.front_default}
+              ></PokemonBox>
+            );
+          })}
+        </div>
+      </main>
+    </>
   );
 }
 
